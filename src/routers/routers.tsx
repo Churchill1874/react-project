75cb633084baaef832142db00f0764ce3506b502
churchill <asdf@@123>
2024-06-25 00:36:27 +0900

    1

--- src/routers/routers.tsx
+++ src/routers/routers.tsx
@@ -1,25 +1,26 @@
 //所有路由配置文件
-import Home from "@/pages/home/Home"
-import Login from "@/pages/login/Login"
-import Personal from "@/pages/personal/Personal"
+import Home from '@/pages/home/Home';
+import Login from '@/pages/login/Login';
+import Personal from '@/pages/personal/Personal';
 
 export default [
-    //首页
-    {
-        path: "/home",
-        component: Home 
-    },
-    //登陆路由
-    {
-        path: "/login",
-        component: Login 
-    },
-    //个人中心路由
-    {
-        path: "/personal",
-        component: Personal 
-    }
-]
-
-
-
+  //首页
+  {
+    path: '/',
+    component: Home,
+  },
+  {
+    path: '/home',
+    component: Home,
+  },
+  //登陆路由
+  {
+    path: '/login',
+    component: Login,
+  },
+  //个人中心路由
+  {
+    path: '/personal',
+    component: Personal,
+  },
+];
