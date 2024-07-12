874f8ddacc951c75ae1f1fa579ab29bd1832ff73
churchill <asdf@@123>
2024-07-13 05:24:39 +0900

    1

--- src/components/news/NewsList.tsx
+++ src/components/news/NewsList.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import NewsRecord from '@/components/news/NewsRecord';
 import { DotLoading, InfiniteScroll } from 'antd-mobile';
 import { Request_NewsPage, NewsPageRequestType, NewsInfoType } from '@/pages/news/api';
@@ -7,119 +7,237 @@ import useStore from '@/zustand/store'
 
 
 const NewsScrollContent = ({ hasMore }: { hasMore?: boolean }) => {
-    return (
+  return (
+    <>
+      {hasMore ? (
         <>
-            {hasMore ? (
-                <>
-                    <div className="dot-loading-custom" >
-                        <span >Loading</span>
-                        <DotLoading color='#fff'/>
-                    </div>
-                </>
-            ) : (
-                <span color='#fff'>--- 我是有底线的 ---</span>
-            )}
+          <div className="dot-loading-custom" >
+            <span >Loading</span>
+            <DotLoading color='#fff' />
+          </div>
         </>
-    )
+      ) : (
+        <span color='#fff'>--- 我是有底线的 ---</span>
+      )}
+    </>
+  )
 }
 
+
+
 const NewsList: React.FC<any> = ({ newsTab }) => {
-    const [newsType, setNewsType] = useState("1");
-    const [hasMore, setHasMore] = useState(true)
-    const [page, setPage] = useState(1);//分页号码
+  console.log('newsTab:', newsTab)
+  //各种新闻页面是否获取到了下一页的状态数据
+  const [newsHasMore, setNewsHasMore] = useState(true)
+  const [sportHasMore, setSportHasMore] = useState(true)
+  const [entertainmentHasMore, setEntertainmentHasMore] = useState(true)
+  const [militaryHasMore, setMilitaryHasMore] = useState(true)
+  const [scienceHasMore, setScienceHasMore] = useState(true)
+  const [netFriendHasMore, setNetFriendHasMore] = useState(true)
 
-    const { newsList, setNewsList,
-        sportList, setSportList,
-        entertainmentList, setEntertainmentList,
-        militaryList, setMilitaryList,
-        scienceList, setScienceList,
-        favorList, setFavorList,
-        netFriendList, setNetFriendList } = useStore();//新闻的全局变量
 
-    // 模拟请求不同类型的新闻数据
-    const reqNewsApi = async (categoryEnum: string, page: number) => {
-        const pageReq: NewsPageRequestType = { pageNum: page, pageSize: 10, categoryEnum: categoryEnum };
-        const newsListResp: NewsInfoType[] = (await Request_NewsPage(pageReq)).data.records || [];
+  const [newsPage, setNewsPage] = useState(1);//新闻页当前分页号码
+  const [sportPage, setSportPage] = useState(1);//运动页当前分页号码
+  const [entertainmentPage, setEntertainmentPage] = useState(1);//娱乐页当前分页号码
+  const [militaryPage, setMilitaryPage] = useState(1);//军事页当前分页号码
+  const [sciencePage, setSciencePage] = useState(1);//科技页当前分页号码
+  const [netFriendPage, setNetFriendPage] = useState(1);//网友分页号码
 
-        setHasMore(newsListResp.length > 0)
 
-        //对比查询新闻的类型属于哪个类型数据 并且确认有新的数据返回才修改 全局的数据状态
-        if (categoryEnum === '1' && newsListResp.length > 0) {//新闻
-            setNewsList((prev: NewsInfoType[]) => [...prev, ...newsListResp]);
-        }
-        if (categoryEnum === '2' && newsListResp.length > 0) {//体育
-            setSportList((prev) => [...prev, ...newsListResp]);
-        }
-        if (categoryEnum === '3' && newsListResp.length > 0) {//娱乐
-            setEntertainmentList((prev) => [...prev, ...newsListResp]);
-        }
-        if (categoryEnum === '4' && newsListResp.length > 0) {//军事
-            setMilitaryList((prev) => [...prev, ...newsListResp]);
-        }
-        if (categoryEnum === '5' && newsListResp.length > 0) {//科技
-            setScienceList((prev) => [...prev, ...newsListResp]);
-        }
-        if (categoryEnum === '6' && newsListResp.length > 0) {//人情
-            setFavorList((prev) => [...prev, ...newsListResp]);
-        }
-        if (categoryEnum === '7' && newsListResp.length > 0) {//网友
-            setNetFriendList((prev) => [...prev, ...newsListResp]);
-        }
-    };
+  //各种新闻类型全局状态数据
+  const { newsList, setNewsList,
+    sportList, setSportList,
+    entertainmentList, setEntertainmentList,
+    militaryList, setMilitaryList,
+    scienceList, setScienceList,
+    netFriendList, setNetFriendList } = useStore();//新闻的全局变量
 
-    //获取当前胶囊新闻类型所用的新闻数据状态
-    const capsuleTabData = (): NewsInfoType[] => {
-        if (newsTab === '1') {
-            console.log(newsList)
-            return newsList || [];
+  //获取某种新闻类型当前页面是否还有新数据返回的状态
+  const getPageHasMore = () => {
+    console.log('getPageHasMore', militaryHasMore)
+    if (newsTab === '1') return newsHasMore;
+    if (newsTab === '2') return sportHasMore;
+    if (newsTab === '3') return entertainmentHasMore;
+    if (newsTab === '4') return militaryHasMore;
+    if (newsTab === '5') return scienceHasMore;
+    if (newsTab === '7') return netFriendHasMore;
+    return false;
+  }
+
+
+  //根据新闻类型获取该新闻类型页面的当前分页号码
+  const getTabPageNum = () => {
+    if (newsTab === '1') return newsPage;
+    if (newsTab === '2') return sportPage;
+    if (newsTab === '3') return entertainmentPage;
+    if (newsTab === '4') return militaryPage;
+    if (newsTab === '5') return sciencePage;
+    if (newsTab === '7') return netFriendPage;
+  }
+
+
+  //存储当前新闻类型页面分页号
+  const setTabPageNum = () => {
+    if (newsTab === '1') setNewsPage((prev) => prev + 1);
+    if (newsTab === '2') setSportPage((prev) => prev + 1);
+    if (newsTab === '3') setEntertainmentPage((prev) => prev + 1);
+    if (newsTab === '4') setMilitaryPage((prev) => prev + 1);
+    if (newsTab === '5') setSciencePage((prev) => prev + 1);
+    if (newsTab === '7') setNetFriendPage((prev) => prev + 1);
+  }
+
+
+
+  // 模拟请求不同类型的新闻数据
+  const reqNewsApi = async (categoryEnum: string) => {
+    console.log('新闻更多数据：', newsHasMore)
+    const pageReq: NewsPageRequestType = { pageNum: getTabPageNum(), pageSize: 10, categoryEnum: categoryEnum };
+    const newsListResp: NewsInfoType[] = (await Request_NewsPage(pageReq)).data.records || [];
+    console.log('请求', categoryEnum, '返回', newsListResp)
+
+    //对比查询新闻的类型属于哪个类型数据 并且确认有新的数据返回才修改 全局的数据状态
+    if (newsListResp.length > 0) {
+      if (categoryEnum === '1') {//新闻
+        if (JSON.stringify(newsListResp) !== JSON.stringify(newsList)) {
+          setNewsHasMore(true)
+          setTabPageNum();
+          const data = [...newsList, ...newsListResp];
+          setNewsList(data);
+        } else {
+          setNewsHasMore(false);
         }
-        if (newsTab === '2') {
-            return sportList || [];
+      }
+
+      if (categoryEnum === '2') {//体育
+        if (JSON.stringify(newsListResp) !== JSON.stringify(sportList)) {
+          setSportHasMore(true)
+          setTabPageNum();
+          const data = [...sportList, ...newsListResp];
+          setSportList(data);
+        } else {
+          setSportHasMore(false);
         }
-        if (newsTab === '3') {
-            return entertainmentList || [];
+      }
+
+      if (categoryEnum === '3') {//娱乐
+        if (JSON.stringify(newsListResp) !== JSON.stringify(entertainmentList)) {
+          setEntertainmentHasMore(true)
+          setTabPageNum();
+          const data = [...entertainmentList, ...newsListResp];
+          setEntertainmentList(data);
+        } else {
+          setEntertainmentHasMore(false);
         }
-        if (newsTab === '4') {
-            return militaryList || [];
+      }
+
+      if (categoryEnum === '4') {//军事
+        if (JSON.stringify(newsListResp) !== JSON.stringify(militaryList)) {
+          setMilitaryHasMore(true)
+          setTabPageNum();
+          const data = [...militaryList, ...newsListResp];
+          setMilitaryList(data);
+        } else {
+          setMilitaryHasMore(false);
         }
-        if (newsTab === '5') {
-            return scienceList || [];
+      }
+
+
+      if (categoryEnum === '5') {//科技
+        if (JSON.stringify(newsListResp) !== JSON.stringify(scienceList)) {
+          setScienceHasMore(true)
+          setTabPageNum();
+          const data = [...scienceList, ...newsListResp];
+          setScienceList(data);
+        } else {
+          setScienceHasMore(false);
         }
-        if (newsTab === '6') {
-            return favorList || [];
+      }
+
+      if (categoryEnum === '7') {//网友
+        if (JSON.stringify(newsListResp) !== JSON.stringify(netFriendList)) {
+          setNetFriendHasMore(true)
+          setTabPageNum();
+          const data = [...netFriendList, ...newsListResp];
+          setNetFriendList(data);
+        } else {
+          setNetFriendHasMore(false);
         }
-        if (newsTab === '7') {
-            return netFriendList || [];
-        }
-        return [];
+      }
+
+    } else {
+      if (categoryEnum === '1') {//新闻
+        setNewsHasMore(false)
+      }
+      if (categoryEnum === '2') {//体育
+        setSportHasMore(false)
+      }
+      if (categoryEnum === '3') {//娱乐
+        setEntertainmentHasMore(false)
+      }
+      if (categoryEnum === '4') {//军事
+        setMilitaryHasMore(false)
+      }
+      if (categoryEnum === '5') {//科技
+        setScienceHasMore(false)
+      }
+      if (categoryEnum === '7') {//网友
+        setNetFriendHasMore(false)
+      }
     }
 
-    const loadMore = () => {
-        setPage((prev) => prev + 1);
-        reqNewsApi(newsTab, page + 1);
+  };
+
+
+  //获取当前胶囊新闻类型所用的新闻数据状态
+  const capsuleTabData = (): NewsInfoType[] => {
+    if (newsTab === '1') {
+      return newsList || [];
     }
+    if (newsTab === '2') {
+      return sportList || [];
+    }
+    if (newsTab === '3') {
+      return entertainmentList || [];
+    }
+    if (newsTab === '4') {
+      return militaryList || [];
+    }
+    if (newsTab === '5') {
+      return scienceList || [];
+    }
+    if (newsTab === '7') {
+      return netFriendList || [];
+    }
+    return [];
+  }
 
-    
-    return (
-        <div className="outer-container">
-            {capsuleTabData() && capsuleTabData()?.map((news, index) => (
-                <NewsRecord
-                    key={index}
-                    title={news.title}
-                    photoPath={news.photoPath}
-                    likesCount={news.likesCount}
-                    badCount={news.badCount}
-                    commentsCount={news.commentsCount}
-                    viewCount={news.viewCount}
-                    createTime={news.createTime}
-                />
-            ))}
-            <InfiniteScroll loadMore={()=>reqNewsApi(newsTab, page)} hasMore={hasMore}>
-                <NewsScrollContent hasMore={hasMore} />
-            </InfiniteScroll>
-        </div>
 
-    );
+  /*   useEffect(() => {
+      reqNewsApi(newsTab);
+    }, [newsTab])
+   */
+
+  return (
+    <div className="outer-container">
+      {capsuleTabData()?.map((news, index) => (
+        <NewsRecord
+          key={index}
+          title={news.title}
+          photoPath={news.photoPath}
+          likesCount={news.likesCount}
+          badCount={news.badCount}
+          commentsCount={news.commentsCount}
+          viewCount={news.viewCount}
+          createTime={news.createTime}
+        />
+      ))}
+      <InfiniteScroll loadMore={() => reqNewsApi(newsTab)} hasMore={getPageHasMore()}>
+        <NewsScrollContent hasMore={getPageHasMore()} />
+      </InfiniteScroll>
+    </div>
+
+  );
 };
+
 
 export default NewsList;
--- src/pages/news/News.tsx
+++ src/pages/news/News.tsx
@@ -1,76 +1,53 @@
+import React, { useState } from "react";
 import { Tabs, CapsuleTabs } from "antd-mobile";
-import { useState, useEffect } from "react";
-import '@/pages/news/News.less'
-import NewsList from '@/components/news/NewsList'
+import '@/pages/news/News.less';
+import NewsList from '@/components/news/NewsList';
 
-const News: React.FC = () => {
-    const [activeKey, setActiveKey] = useState<string>('news');
-    const [activeTab, setActiveTab] = useState<string>('1'); //新闻类型胶囊
+const News: React.FC = React.memo(() => {
+  const [activeKey, setActiveKey] = useState<string>('news');
+  const [activeTab, setActiveTab] = useState<string>('1');
 
+  const handleTabChange = (key: string) => {
+    setActiveKey(key);
+    if (key === 'news') {
+      setActiveTab('1');
+    }
+  };
 
-    //切换新闻菜单导航
-    const handleTabChange = (key: string) => {
-        if (key === 'news') {
-            setActiveTab(activeTab)
-        }
-        if (key === 'abroad') {
-            //setNewsList(null)
-        }
-        if (key === 'political') {
-            //setNewsList(null)
+  const handleCapsuleTabChange = ((key: string) => {
+    setActiveTab(key);
+  });
 
-        }
-        if (key === 'exposure') {
-            //setNewsList(null)
+  return (
+    <>
+      <div className="tabs-container">
+        <Tabs activeKey={activeKey} onChange={handleTabChange}>
+          <Tabs.Tab title='新闻' key='news' />
+          <Tabs.Tab title='境外' key='abroad' />
+          <Tabs.Tab title='曝光' key='exposure' />
+          <Tabs.Tab title='公司' key='political' />
+          <Tabs.Tab title='油管' key='youtube' />
+        </Tabs>
+      </div>
 
-        }
-        if (key === 'youtube') {
-            //setNewsList(null)
+      {activeKey === 'news' &&
+        <div className="capsule-tabs-container">
+          <CapsuleTabs activeKey={activeTab} onChange={handleCapsuleTabChange}>
+            <CapsuleTabs.Tab title="新闻" key="1" />
+            <CapsuleTabs.Tab title="体育" key="2" />
+            <CapsuleTabs.Tab title="娱乐" key="3" />
+            <CapsuleTabs.Tab title="军事" key="4" />
+            <CapsuleTabs.Tab title="科技" key="5" />
+            <CapsuleTabs.Tab title="网友" key="7" />
+          </CapsuleTabs>
+        </div>
+      }
 
-        }
-        setActiveKey(key);
-    };
-
-
-    //切换新闻类型
-    const handleCapsuleTabChange = (key: string) => {
-        setActiveTab(key);
-    };
-
-    return (
-        <>
-            <div className="tabs-container">
-                <Tabs activeKey={activeKey} onChange={handleTabChange}>
-                    <Tabs.Tab title='新闻' key='news' />
-                    <Tabs.Tab title='境外' key='abroad' />
-                    <Tabs.Tab title='曝光' key='exposure' />
-                    <Tabs.Tab title='公司' key='political' />
-                    <Tabs.Tab title='油管' key='youtube' />
-                </Tabs>
-            </div>
-
-
-            {activeKey === 'news' &&
-                <div className="capsule-tabs-container">
-                    <CapsuleTabs activeKey={activeTab} onChange={handleCapsuleTabChange}>
-                        <CapsuleTabs.Tab title="新闻" key="1" />
-                        <CapsuleTabs.Tab title="体育" key="2" />
-                        <CapsuleTabs.Tab title="娱乐" key="3" />
-                        <CapsuleTabs.Tab title="军事" key="4" />
-                        <CapsuleTabs.Tab title="科技" key="5" />
-                        <CapsuleTabs.Tab title="网友" key="7" />
-                    </CapsuleTabs>
-                </div>
-            }
-
-   
-            <div className="news-content">
-                {activeKey === 'news' && <NewsList newsTab={activeTab} />}
-            </div>
-        </>
-    );
-
-}
-
+      <div className="news-content">
+        {activeKey === 'news' && <NewsList newsTab={activeTab} />}
+      </div>
+    </>
+  );
+});
 
 export default News;
